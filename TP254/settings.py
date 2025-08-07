06b69b91907e5c08ba91b95d06e7d313ff458bae
Noble-mungu <64100418+Noble-mungu@users.noreply.github.com>
2025-08-07 11:41:17 +0300

    Add gunicorn

--- TP254/settings.py
+++ TP254/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$3mqzjl#^45^#7ioqa)=2lfe3v(d3kz#bq_ly+zh5_80kp!%d(')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
